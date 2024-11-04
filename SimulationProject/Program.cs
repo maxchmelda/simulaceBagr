b437fc479c046f05a1b269acd667e0c971192a89
Lumír <muller.martin.mm@gmail.com>
2024-11-04 16:39:41 +0100

    Nahrávám

--- SimulationProject/Program.cs
+++ SimulationProject/Program.cs
@@ -1,10 +1,28 @@
 ﻿namespace SimulationProject
 {
+    class Auto
+    {
+        int autoID;
+        int nosnost;//kilogramy
+        int dobaCesty;//min
+        int dobaNakládání; //min
+        int dobaVykládání; //min
+        Stav stav;
+        int časDoDokončeníČinnosti; //min
+        enum Stav { čeká, nakládá, jede, vykládá }
+
+        void Nakládání(){ }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Auto> seznamAut = new List<Auto>();
+            Queue<Auto> NakládacíStanice = new Queue<Auto>();
+            Console.WriteLine("Jak dlouhá je cesta?");
+            int vzdálenostCesty = Convert.ToInt32(Console.ReadLine());
+
+
         }
     }
 }
