e5f827cd35c0eb930fce05130bb9853f37e4964e
Lumir <muller.martin.mm@gmail.com>
2024-11-12 18:22:19 +0100

    A je to

--- SimulationProject/Program.cs
+++ SimulationProject/Program.cs
@@ -9,6 +9,7 @@ namespace SimulationProject
         public int dobaCesty; // min
         public int dobaNakládání; // min
         public int dobaVykládání; // min
+        public int přejezd = 0; //počet kolikrát auto přejelo cestu
 
         public Auto(int nosnost1, int dobaCesty1, int dobaNakládání1, int dobaVykládání1)
         {
@@ -46,18 +47,34 @@ namespace SimulationProject
             Console.WriteLine("Kolik chceš písku?");
             int písek = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Kolik chceš aut?");
-            int početAut = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Typ zadání?\n1 Několik různých aut, chci vědět čas\n 2 Několik stejných aut, chci vědět kolik");
 
-            Console.WriteLine("Jaký je tvoje auto?\nNosnost, Doba cesty, Doba nakládání, Doba vykládání\nKaždý údaj odděl mezerou");
-            for (int i = 1; i <= početAut; i++)
+            int výběrZadání = Convert.ToInt32(Console.ReadLine());
+
+            switch (výběrZadání) 
             {
-                Console.WriteLine("Tvoje " + i + ". auto:");
-                int[] input = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
-                Auta.Add(new Auto(input[0], input[1], input[2], input[3]));
+                case 1:
+                    Console.WriteLine("Kolik chceš aut?");
+                    int početAut = Convert.ToInt32(Console.ReadLine());
 
-                Kalendář.Enqueue(new Akce(i - 1, Akce.Typ.nalož, 0), 0);
+                    Console.WriteLine("Jaký je tvoje auto?\nNosnost, Doba cesty, Doba nakládání, Doba vykládání\nKaždý údaj odděl mezerou");
+                    for (int i = 1; i <= početAut; i++)
+                    {
+                        Console.WriteLine("Tvoje " + i + ". auto:");
+                        int[] input = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+                        Auta.Add(new Auto(input[0], input[1], input[2], input[3]));
+
+                        Kalendář.Enqueue(new Akce(i - 1, Akce.Typ.nalož, 0), 0);
+                    }
+                    break;
+                case 2:
+                    Console.WriteLine("Jaký je tvoje auto?\nNosnost, Doba cesty, Doba nakládání, Doba vykládání\nKaždý údaj odděl mezerou");
+                        int[] input2 = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+                        Auta.Add(new Auto(input2[0], input2[1], input2[2], input2[3]));
+                        Kalendář.Enqueue(new Akce(0, Akce.Typ.nalož, 0), 0);
+                    break;
             }
+            
 
             int finalniCas = 0;
 
@@ -68,14 +85,18 @@ namespace SimulationProject
                 Akce aktualniAkce = Kalendář.Dequeue();
                 Auto aktualniAuto = Auta[aktualniAkce.autoId];
 
+                
                 // pokud jeho akce je naloz tak ho posli vylozit, tzn pridej jeho akci vylozeni s zacatecnim casem: zacatek nalozeni + doba nalozeni + doba cesty 
                 if (aktualniAkce.typ == Akce.Typ.nalož)
                 {
+                    aktualniAuto.přejezd++;
                     int novyZacatek = aktualniAkce.zacatek + aktualniAuto.dobaNakládání + aktualniAuto.dobaCesty;
                     Kalendář.Enqueue(new Akce(aktualniAkce.autoId, Akce.Typ.vylož, novyZacatek), novyZacatek);
                 }
                 else
                 {
+
+                    aktualniAuto.přejezd++;
                     // pokud jeho akce je vyloz tak ho posli nalozit
                     int novyZacatek = aktualniAkce.zacatek + aktualniAuto.dobaVykládání + aktualniAuto.dobaCesty;
                     int pisekKodobrani = Math.Min(aktualniAuto.nosnost, písek);
@@ -83,10 +104,10 @@ namespace SimulationProject
                     // odeber pisek
                     písek -= pisekKodobrani;
 
-                    // pokud uz neni pisek tak finalni cas je cas kdy zacne nakladat znova
+                    // pokud uz neni pisek tak finalni cas je cas kdy dokončil vykládání
                     if (písek <= 0)
                     {
-                        finalniCas = novyZacatek;
+                        finalniCas = aktualniAkce.zacatek + aktualniAuto.dobaVykládání;
                         break;
                     }
 
@@ -119,7 +140,23 @@ namespace SimulationProject
                 }
             }
 
-            Console.WriteLine($"Finální čas je {finalniCas}");
+            switch (výběrZadání) 
+            {
+                case 1:
+                    Console.WriteLine($"Finální čas je {finalniCas}");
+                    int ii = 1;
+                    foreach (Auto auto in Auta)
+                    {
+                        Console.WriteLine(ii + ". auto přejelo cestu " + auto.přejezd + "x");
+                        ii++;
+                    }
+                    break;
+                case 2:
+                    Console.WriteLine("Byl by potřeba konvoj z " + Auta[0].přejezd/2 + " aut.");
+                    break;
+            }
+
+            
         }
     }
 }
