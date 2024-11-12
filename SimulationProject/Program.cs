28e5bb26cfe65f0e52ccf6e5bc06421af6561979
Max Chmelicek <max.chmelicek@gmail.com>
2024-11-12 17:26:06 +0100

    oprava logiky

--- SimulationProject/Program.cs
+++ SimulationProject/Program.cs
@@ -1,15 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace SimulationProject
 {
     class Auto
     {
-        public int nosnost;//kilogramy
-        public int dobaCesty;//min
-        public int dobaNakládání; //min
-        public int dobaVykládání; //min
-
-        void Nakládání(){ }
+        public int nosnost; // kilogramy
+        public int dobaCesty; // min
+        public int dobaNakládání; // min
+        public int dobaVykládání; // min
 
         public Auto(int nosnost1, int dobaCesty1, int dobaNakládání1, int dobaVykládání1)
         {
@@ -25,16 +24,17 @@ namespace SimulationProject
         public int autoId;
         public Typ typ;
         public int zacatek;
-        public Akce(int id, Typ typp, int zacatekk) 
+
+        public Akce(int id, Typ typp, int zacatekk)
         {
             autoId = id;
             typ = typp;
             zacatek = zacatekk;
-
         }
-        public enum Typ { nalož, vylož }
 
+        public enum Typ { nalož, vylož }
     }
+
     internal class Program
     {
         static void Main(string[] args)
@@ -42,91 +42,74 @@ namespace SimulationProject
             List<Auto> Auta = new List<Auto>();
             PriorityQueue<Akce, int> Kalendář = new PriorityQueue<Akce, int>();
 
-
-            Console.WriteLine("Kolik chceš pysku");
+            Console.WriteLine("Kolik chceš písku?");
             int písek = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("Kolik chceš aut?");
             int početAut = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Jaký je tvoje auto?\n Nosnost, Doba cesty, Doba nakládání, Doba vykládábní\n Každý údaj odděl mezerou");
-            int startTime = 0;
-            for (int i = 1; i <= početAut; i++) 
+            Console.WriteLine("Jaký je tvoje auto?\nNosnost, Doba cesty, Doba nakládání, Doba vykládání\nKaždý údaj odděl mezerou");
+            for (int i = 1; i <= početAut; i++)
             {
-                Console.WriteLine("Tvoje "+ i +". auto:");
+                Console.WriteLine("Tvoje " + i + ". auto:");
                 int[] input = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
                 Auta.Add(new Auto(input[0], input[1], input[2], input[3]));
 
-                Kalendář.Enqueue(new Akce(i - 1, Akce.Typ.nalož, startTime), startTime);
-                startTime = Auta[i - 1].dobaNakládání;
+                Kalendář.Enqueue(new Akce(i - 1, Akce.Typ.nalož, 0), 0);
             }
 
             int finalniCas = 0;
 
             while (písek > 0)
             {
-                Akce aktualníAkce = Kalendář.Dequeue();
-                Auto aktuálníAuto = Auta[aktualníAkce.autoId];
+                Akce aktualniAkce = Kalendář.Dequeue();
+                Auto aktualniAuto = Auta[aktualniAkce.autoId];
 
-                if (aktualníAkce.typ == Akce.Typ.nalož)
+                if (aktualniAkce.typ == Akce.Typ.nalož)
                 {
-                    int novýZačátek = aktualníAkce.zacatek + aktuálníAuto.dobaNakládání + aktuálníAuto.dobaCesty;
-                    Kalendář.Enqueue(new Akce(aktualníAkce.autoId, Akce.Typ.vylož, novýZačátek), novýZačátek);
+                    int novyZacatek = aktualniAkce.zacatek + aktualniAuto.dobaNakládání + aktualniAuto.dobaCesty;
+                    Kalendář.Enqueue(new Akce(aktualniAkce.autoId, Akce.Typ.vylož, novyZacatek), novyZacatek);
                 }
-                else 
+                else
                 {
-                    int novýZačátek = aktualníAkce.zacatek + aktuálníAuto.dobaVykládání + aktuálníAuto.dobaCesty;
+                    int novyZacatek = aktualniAkce.zacatek + aktualniAuto.dobaVykládání + aktualniAuto.dobaCesty;
+                    int pisekKodobrani = Math.Min(aktualniAuto.nosnost, písek);
 
-                    List<Akce> akceVKalendari = new List<Akce>();
+                    písek -= pisekKodobrani;
 
-                    foreach (var akce in Kalendář.UnorderedItems)
+                    if (písek <= 0)
                     {
-                        akceVKalendari.Add(akce.Element);
+                        finalniCas = novyZacatek;
+                        break;
                     }
 
+                    List<Akce> akceVKalendari = new List<Akce>(Kalendář.UnorderedItems.Select(a => a.Element));
                     int posledniCasNalozeni = 0;
 
                     foreach (var akceVKal in akceVKalendari)
                     {
-                        if (akceVKal.zacatek + Auta[akceVKal.autoId].dobaNakládání > posledniCasNalozeni)
+                        if (akceVKal.typ == Akce.Typ.nalož)
                         {
-                            posledniCasNalozeni = akceVKal.zacatek + Auta[akceVKal.autoId].dobaNakládání;
+                            int casNalozeni = akceVKal.zacatek + Auta[akceVKal.autoId].dobaNakládání;
+                            if (casNalozeni > posledniCasNalozeni)
+                            {
+                                posledniCasNalozeni = casNalozeni;
+                            }
                         }
                     }
 
-
-                    int pisekKodebrani = aktuálníAuto.nosnost;
-
-                    if (posledniCasNalozeni < novýZačátek) {
-                        Kalendář.Enqueue(new Akce(aktualníAkce.autoId, Akce.Typ.nalož, novýZačátek), novýZačátek);
-                        if (písek - pisekKodebrani <= 0)
-                        {
-                            finalniCas = novýZačátek + aktuálníAuto.dobaNakládání;
-                            break;
-                        }
-                        else
-                        {
-                            písek -= pisekKodebrani;
-                        }
-                    } else
+                    if (posledniCasNalozeni < novyZacatek)
                     {
-                        if (písek - pisekKodebrani <= 0)
-                        {
-                            finalniCas = novýZačátek + posledniCasNalozeni;
-                            break;
-                        }
-                        else
-                        {
-                            písek -= pisekKodebrani;
-                        }
-                        Kalendář.Enqueue(new Akce(aktualníAkce.autoId, Akce.Typ.nalož, posledniCasNalozeni), posledniCasNalozeni);
+                        Kalendář.Enqueue(new Akce(aktualniAkce.autoId, Akce.Typ.nalož, novyZacatek), novyZacatek);
                     }
-                    
+                    else
+                    {
+                        Kalendář.Enqueue(new Akce(aktualniAkce.autoId, Akce.Typ.nalož, posledniCasNalozeni), posledniCasNalozeni);
+                    }
                 }
-
             }
 
-            Console.WriteLine($"Finalni cas je {finalniCas}");
+            Console.WriteLine($"Finální čas je {finalniCas}");
         }
     }
 }
